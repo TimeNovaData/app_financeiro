f2a97b025071fdac161195d1e83e94d3d1890dee
flaviotech <flavio.nogueira.profissional@gmail.com>
2023-01-24 06:30:42 -0300

    Retirando serializers e templates

--- serializers/__init__.py
+++ serializers/__init__.py
@@ -7,26 +7,23 @@ from .conta_nivel4_serializer import ContaNivel4Serializer
 from .dado_bancario_serializer import DadoBancarioSerializer
 from .forma_pagamento_serializer import FormaPagamentoSerializer
 from .lancamento_financeiro_serializer import LancamentoFinanceiroSerializer
-from .item_rateio_serializer import ItemRateioSerializer
 from .parcela_serializer import ParcelaSerializer
-from .rateio_serializer import RateioSerializer
 from .tipo_conta_serializer import TipoContaSerializer
-from .tipo_documento_financeiro_serializer import \
-    TipoDocumentoFinanceiroSerializer
+from .tipo_documento_financeiro_serializer import (
+    TipoDocumentoFinanceiroSerializer,
+)
 
-__all__ =[
+__all__ = [
     CaixaBancoSerializer,
     ContaNivel1Serializer,
     ContaNivel2Serializer,
     ContaNivel3Serializer,
     TipoDocumentoFinanceiroSerializer,
-	LancamentoFinanceiroSerializer,
-	ContaNivel4Serializer,
-	DadoBancarioSerializer,
-	FormaPagamentoSerializer,
-	ItemRateioSerializer,
-	ParcelaSerializer,
-	RateioSerializer,
-	TipoContaSerializer,
-	BancoSerializer,
+    LancamentoFinanceiroSerializer,
+    ContaNivel4Serializer,
+    DadoBancarioSerializer,
+    FormaPagamentoSerializer,
+    ParcelaSerializer,
+    TipoContaSerializer,
+    BancoSerializer,
 ]
--- signals/__init__.py
+++ signals/__init__.py
@@ -1,11 +1,9 @@
-from .item_rateio_signals import item_rateio_post_save
 from .lancamento_financeiro_signals import (
     lancamento_financeiro_post_delete,
     lancamento_financeiro_post_save,
 )
 
 __all__ = [
-    item_rateio_post_save,
     lancamento_financeiro_post_delete,
     lancamento_financeiro_post_save,
 ]
